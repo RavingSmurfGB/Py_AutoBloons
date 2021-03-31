e137edb8434802bce239f5625e710ceed8cbcc66
RavingSmurfGB <josephhutchinson4@gmail.com>
2021-03-31 15:08:58 +0100

    Implemented setup and readme

---
+++ Setup.py
@@ -0,0 +1,4 @@
+import subprocess
+
+print("Installing requirments")
+subprocess.call('cmd /c "pip install -r requirtements.txt"') ## doesnt wooooooooooooooooooooooooooooooooooooooooooooooooooooooork!
